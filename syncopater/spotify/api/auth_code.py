b4f4f9ba7d3a304f844ec2ed9f1d046133e5f213
Justin Martin <justinmartin50@gmail.com>
2023-12-19 21:24:24 -0500

    auth session store

--- syncopater/spotify/api/auth_code.py
+++ syncopater/spotify/api/auth_code.py
@@ -1,6 +1,7 @@
 import webbrowser
 from urllib.parse import urlencode 
 from .utils import get_secrets
+from django.conf import settings
 
 def get_auth_code():
     client_id, client_secret = get_secrets()
@@ -8,10 +9,10 @@ def get_auth_code():
     auth_headers = {
         "client_id": client_id,
         "response_type": "code",
-        "redirect_uri": "https://www.syncopater.com/spotify/auth_code/",
+        "redirect_uri": f"{settings.BASE_URL}/spotify/callback/",
         "scope": "user-library-read"
     }
 
-    webbrowser.open("https://accounts.spotify.com/authorize?" + urlencode(auth_headers))
+    webbrowser.open("https://accounts.spotify.com/authorize?" + urlencode(auth_headers), new=0)
 
     return
